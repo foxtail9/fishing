b0ab68a7d9552479b2d6fed8ce0ab71e4a5f84b5
udiladido <89720389+udiladido@users.noreply.github.com>
2024-09-10 16:49:45 +0900

    Add
    게임 레벨 추가 및 수정

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -4,25 +4,75 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class SceneController: MonoBehaviour
 {
 
-    public int NowStage_level;
+    public int NowStage_level = 1;
     public int MaxStage_level = 3;
     public Text NowStage_Text;
+
+    public Text UnlockStage;
+
+    public GameObject StageLev;
+    public GameObject StageLock;
+
+    int isUnLock;
+
+    enum Achive { unlock2, unlock3, unlock4}
+    Achive[] achives;
+
+
+
+    void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive));
+
+        if (!PlayerPrefs.HasKey("MyData"))
+            init();
+
+    }
+
+
+    void Start()
+    {
+    
+        
+
+    }
+
+
+
+    void init()
+    {
+
+        //스테이지 0이면 잠긴것, 1이면 이전에 클리어 한것
+        PlayerPrefs.SetInt("MyData", 1);
+
+        foreach (Achive achive in achives)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 0);
+        
+        }
+
+
+    }
+    
+
 
 
 
     public void Update()
     {
         NowStage_Text.text = $"스테이지 {NowStage_level}  /  {MaxStage_level}";
-
-
-
+        UnlockStage.text = NowStage_level.ToString();
 
     }
 
+
+    
+ 
 
 
     public void Left_Stage(){ 
@@ -47,12 +97,41 @@ public class SceneController: MonoBehaviour
 
     public void LoadLevel()
     {
-        SceneManager.LoadScene($"GameScene{NowStage_level}");
+        SceneManager.LoadScene($"GameScene {NowStage_level}");
     
 
 
     }
 
 
+    public void UnlockState()
+    {
+
+
+
+        String ahiveName = achives[NowStage_level].ToString();
+        isUnLock = PlayerPrefs.GetInt(ahiveName);
+
+        if (isUnLock == 0)
+        {
+            StageLock.SetActive(false);
+            StageLev.SetActive(true);
+
+        }
+
+        else
+        {
+            StageLock.SetActive(true);
+            StageLev.SetActive(false);
+
+        }
+
+
+    }
+
+
+
+    // 게임 클리어 시 정보를 저장하는 내용 만들어야.
+    
 
 }
