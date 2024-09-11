c7543701909226b0d10329757ae449d00deb09f4
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-09-11 15:49:38 +0900

    FIX audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
+
     AudioSource audioSource;
     public AudioClip clip;
 
-
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         audioSource = GetComponent<AudioSource>();  
-        audioSource.clip = this.clip;    
-        audioSource.Play(); 
+        audioSource.clip = this.clip;
+        audioSource.Play();
     }
 }
