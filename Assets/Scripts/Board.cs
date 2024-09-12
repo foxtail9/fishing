71ed18c95faa2d02c35cc05368efb7c25b47b8ff
udiladido <89720389+udiladido@users.noreply.github.com>
2024-09-12 15:05:12 +0900

    feat
    스테이지 별 카드 개수 변경

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
-
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
 
 public class Board : MonoBehaviour
 {
+
+    public int Max_Stage = 3;
+    public int Stage_CardNum;
+    public int stageLevel;
+
     public GameObject card;
 
     [HideInInspector]
     public bool isCardDistributed;
-
+      
     Dictionary<GameObject, Vector2> cardList = new Dictionary<GameObject, Vector2>();
 
 
@@ -30,23 +36,31 @@ public class Board : MonoBehaviour
 
     public void GenerateCard()
     {
-        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
-        arr = arr.OrderBy(x => Random.Range(0f, 7f)).ToArray();
 
-        for (int i = 0; i < 16; i++)
+        int[] arr = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
+        StageCardNum();
+
+        Debug.Log(stageLevel);
+        int[] stage_arr = arr.Take(Stage_CardNum).ToArray();
+        
+        stage_arr = stage_arr.OrderBy(x => Random.Range(0f, Stage_CardNum)).ToArray();
+
+
+
+        for (int i = 0; i < Stage_CardNum; i++)
         {
             GameObject go = Instantiate(card, this.transform);
             go.transform.position = new Vector2(0f, -0.91f);
 
-            float x = (i / 4) * 1.4f - 2.1f;
-            float y = (i % 4) * 1.4f - 3.0f;
+            float x = (i % 4) * 1.4f - 2.1f;
+            float y = (i / 4) * 1.4f - 3.0f;
 
             Vector2 targetPosition = new Vector2(x, y);
             cardList.Add(go, targetPosition);
 
             go.GetComponent<Card>().Setting(arr[i]);
         }
-        GameManager.Instance.cardCount = arr.Length;
+        GameManager.Instance.cardCount = stage_arr.Length;
     }
 
     IEnumerator DistributeCard(float waitSeconds)
@@ -64,4 +78,23 @@ public class Board : MonoBehaviour
         yield return new WaitForSeconds(0.7f);
         isCardDistributed = true;
     }
+
+    public void StageCardNum()
+    {
+
+        Scene scene;
+        scene = SceneManager.GetActiveScene();
+        string stageName = scene.name;
+        string tempStr = Regex.Replace(stageName, @"\D", "");
+        stageLevel = int.Parse(tempStr);
+
+
+        Stage_CardNum = 1 << stageLevel +1;
+
+
+
+
+    }
+
+
 }
